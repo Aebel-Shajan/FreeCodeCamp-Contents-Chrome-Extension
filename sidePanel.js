bea9bca334490d6be94272380378127c7bdfe618
Aebel Shajan <aebelshajan@gmail.com>
2023-12-23 20:00:30 +0000

    updated css of extension to match fcc website

--- sidePanel.js
+++ sidePanel.js
@@ -1,1 +1,12 @@
-contentList = document.body.querySelector("#contents-list");
+import curriculum from './webscraping/curriculum.json' assert { type: 'json' };
+
+let contentList = document.body.querySelector("#contents-list");
+let linkTemplate = document.body.querySelector("#link-template");
+console.log(curriculum)
+
+for (let course of curriculum) {
+	let link = linkTemplate.content.cloneNode(true);
+	link.querySelector('.contents-link').setAttribute("href", course["course link"]);
+	link.querySelector('.contents-item').innerText = course["course title"];
+	contentList.appendChild(link);
+}

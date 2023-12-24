16fdf12171f4e35c9826860a4d22b1bac01d5c13
Aebel Shajan <aebelshajan@gmail.com>
2023-12-24 15:43:51 +0000

    implemented tab options

--- sidePanel.js
+++ sidePanel.js
@@ -1,5 +1,19 @@
 import curriculum from './webscraping/curriculum.json' assert { type: 'json' };
 
+let toggleButtons = document.querySelectorAll(".toggle-buttons");
+toggleButtons.forEach((option) => {
+	let buttons = option.querySelectorAll(".fcc-button");
+	buttons.forEach((button) => {
+		button.addEventListener('click', () => {
+			buttons.forEach((btn) => {
+				btn.classList.remove("enabled");
+			})
+			button.classList.add('enabled');
+			option.id = button.innerText.toLowerCase();
+		})
+	})
+})
+
 let contentList = document.body.querySelector("#contents-list");
 let linkTemplate = document.body.querySelector("#link-template");
 let groupId = null;
@@ -18,11 +32,11 @@ function updateSidePanel() {
 	let courseIndex = curriculum.indexOf(focusedCourse);
 	let chapterIndex = focusedCourse["chapters"].indexOf(focusedChapter);
 	let focusedCourseItem = createContentItem(
-		[courseIndex, " "+focusedCourse["course title"]].join("."),
+		[courseIndex, " " + focusedCourse["course title"]].join("."),
 		focusedCourse["course link"]
 	);
 	let focusedChapterItem = createContentItem(
-		[courseIndex, chapterIndex, " "+focusedChapter["chapter title"]].join("."),
+		[courseIndex, chapterIndex, " " + focusedChapter["chapter title"]].join("."),
 		focusedChapter["chapter link"]
 	);
 	focusedCourseItem.querySelector('.fa-solid').className = "fa-solid fa-circle-minus";
@@ -50,7 +64,7 @@ function updateSidePanel() {
 			for (let i = 0; i < focusedChapter["lessons"].length; i++) {
 				let lesson = focusedChapter["lessons"][i];
 				let lessonItem = createContentItem(
-					[courseIndex, chapterIndex, i, " "+lesson["lesson title"]].join("."),
+					[courseIndex, chapterIndex, i, " " + lesson["lesson title"]].join("."),
 					lesson["lesson link"]
 				);
 				lessonItem.querySelector("li").style.paddingLeft = "3.0rem";
@@ -64,7 +78,7 @@ function updateSidePanel() {
 			for (let i = 0; i < focusedCourse["chapters"].length; i++) {
 				let chapter = focusedCourse["chapters"][i];
 				let chapterItem = createContentItem(
-					[courseIndex, i, " "+chapter["chapter title"]].join("."),
+					[courseIndex, i, " " + chapter["chapter title"]].join("."),
 					chapter["chapter link"]
 				);
 				chapterItem.querySelector("li").style.paddingLeft = "1.5rem";
@@ -85,7 +99,7 @@ function updateSidePanel() {
 			for (let i = 0; i < curriculum.length; i++) {
 				let course = curriculum[i];
 				let courseItem = createContentItem(
-					[i, " "+course["course title"]].join("."),
+					[i, " " + course["course title"]].join("."),
 					course["course link"]
 				);
 				courseItem.querySelector('.content-expand').addEventListener('click', (event) => {
@@ -111,16 +125,31 @@ function openInTabGroup(event) {
 			groupId = null;
 		}
 	});
-	console.log(event.target.href)
-	chrome.tabs.create({ url: event.target.parentElement.href, active: false }, function (tab) {
+	// 😨 clean in future
+	chrome.tabs.create({ url: event.target.parentElement.href, active: true }, function (tab) {
 		if (groupId === null) {
 			chrome.tabs.group({ tabIds: tab.id }, function (newGroupId) {
 				groupId = newGroupId;
-				chrome.tabGroups.update(groupId, { title: "FCC Research", color: "red" })
+				chrome.tabGroups.update(groupId, { title: "FCC Research", color: "red", collapsed: false})
 			});
 			chrome.tabs.move(tab.id, { index: 0 });
 		} else {
-			chrome.tabs.group({ groupId: groupId, tabIds: tab.id });
+			chrome.tabGroups.update(groupId, {collapsed: false });
+			const overWriteTab = document.querySelector(".common-tabs-toggle").id;
+			let url = new URL(event.target.parentElement.href);
+			let baseUrl = url.protocol + '//' + url.host + '/*';
+			if (overWriteTab === "on") {
+				chrome.tabs.query({ groupId: groupId, url: baseUrl }, function(tabs) {
+					if (tabs.length > 0) {
+						chrome.tabs.remove(tab.id);
+						chrome.tabs.update(tabs[0].id, {url: event.target.parentElement.href, active: true})
+					} else {
+						chrome.tabs.group({ groupId: groupId, tabIds: tab.id });
+					};
+				})
+			} else if (overWriteTab === "off") {
+				chrome.tabs.group({ groupId: groupId, tabIds: tab.id });
+			}
 		}
 	});
 }
@@ -129,6 +158,17 @@ function createContentItem(title, link) {
 	let contentItem = linkTemplate.content.cloneNode(true);
 	contentItem.querySelector('.content-text').innerText = title;
 	contentItem.querySelector('.content-link').setAttribute("href", link);
-	contentItem.querySelector('.content-link').addEventListener('click', openInTabGroup);
+	contentItem.querySelector('.content-link').addEventListener('click', (event) => {
+		const toNewTab = document.querySelector(".tab-toggle").id
+		if (toNewTab === "on") {
+			openInTabGroup(event)
+		} else if (toNewTab === "off") {
+			event.preventDefault();
+			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+				var tab = tabs[0];
+				chrome.tabs.update(tab.id, { url: event.target.parentElement.href });
+			});
+		}
+	});
 	return contentItem
 }
